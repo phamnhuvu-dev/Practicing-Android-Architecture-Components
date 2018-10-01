e016c6ff52e16e26eb21e28f3008a02bd2d125aa
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2018-10-01 15:56:28 +0700

    Navigation component

---
+++ Navigation/app/src/main/java/com/phamnhuvu/navigation/LoginFragment.kt
@@ -0,0 +1,43 @@
+package com.phamnhuvu.navigation
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.NavHostFragment.findNavController
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class LoginFragment : Fragment() {
+
+  private lateinit var mEditText: EditText
+  private lateinit var mButton: Button
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                            savedInstanceState: Bundle?): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_login, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    mEditText = view.findViewById(R.id.editText)
+    mButton = view.findViewById(R.id.button)
+
+    mButton.setOnClickListener {
+      findNavController(this).navigate(R.id.action_loginFragment_to_mainFragment)
+    }
+  }
+}
